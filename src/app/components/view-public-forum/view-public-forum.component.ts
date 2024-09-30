55114191b9290f043cdb5995a28053f7e53e63bc
Pasan-FDo <pasanfernando.work@gmail.com>
2024-09-30 17:21:05 +0530

    reply view and delete component created

--- src/app/components/view-public-forum/view-public-forum.component.ts
+++ src/app/components/view-public-forum/view-public-forum.component.ts
@@ -1,12 +1,110 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { PublicForumService } from '../../services/plant-care/public-forum.service';
+import { error, log } from 'console';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-public-forum',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NgxPaginationModule,
+    FormsModule,
+  ],
   templateUrl: './view-public-forum.component.html',
   styleUrl: './view-public-forum.component.css'
 })
-export class ViewPublicForumComponent {
+export class ViewPublicForumComponent implements OnInit {
+  publiForum!: PublicForum[];
+  isPopupVisible = false;
+  deletePopUpVisible = false;
+  postId!: number;
 
+  constructor(private publicForumSrv: PublicForumService) { }
+
+
+  ngOnInit(): void {
+    this.postId = 1;
+    this.fetchPostAllReply(this.postId);
+  }
+
+  openPopup() {
+    this.isPopupVisible = true;
+  }
+
+  closePopup() {
+    this.isPopupVisible = false;
+  }
+
+  openDeletePopUp() {
+    this.deletePopUpVisible = true;
+  }
+  closeDeletePopUp() {
+    this.deletePopUpVisible = true;
+  }
+
+  fetchPostAllReply(id: number) {
+    this.publicForumSrv.getAllPostReply(id).subscribe(
+      (res) => {
+        console.log(res);
+        this.publiForum = res;
+      },
+      (error) => {
+        console.log("Error: ", error);
+      }
+    )
+
+  }
+
+  deleteReply(id:number){
+    console.log("delete id:",id);
+    
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Do you really want to delete this crop Task item? This action cannot be undone.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'Cancel',
+    }).then((result)=>{
+      if(result.isConfirmed){
+        this.publicForumSrv.deleteReply(id).subscribe(
+          (res:any)=>{
+            if(res){
+              Swal.fire(
+                'Deleted!',
+                'The crop calendar item has been deleted.',
+                'success'
+              );
+              this.isPopupVisible = true;
+              this.fetchPostAllReply(this.postId)
+            }
+          },
+          (error)=>{
+            console.log('Error', error);
+            Swal.fire(
+              'Error!',
+              'There was an error deleting the crop calendar.',
+              'error'
+            );
+          }
+        )
+      }
+    })
+  }
+
+}
+
+
+class PublicForum {
+  "id": number
+  "replyMessage": string
+  "createdAt": string
+  "firstName": string
+  "lastName": string
 }

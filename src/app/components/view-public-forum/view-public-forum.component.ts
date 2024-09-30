cc17f8d3e9bf112084005b0753afea7e151a62fc
bimangunarathne <bimangunarathne@gmail.com>
2024-09-30 12:16:41 +0530

    update the

--- src/app/components/view-public-forum/view-public-forum.component.ts
+++ src/app/components/view-public-forum/view-public-forum.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-view-public-forum',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view-public-forum.component.html',
-  styleUrl: './view-public-forum.component.css'
+  styleUrl: './view-public-forum.component.css',
 })
 export class ViewPublicForumComponent {
-
+  isDeleteVisible = false;
+  toggleDeleteButton() {
+    this.isDeleteVisible = !this.isDeleteVisible;
+  }
 }

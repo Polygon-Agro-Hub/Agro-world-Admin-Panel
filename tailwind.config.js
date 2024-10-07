8fbf9cc9d17b9403f8483e68cda9c883b6837c73
Chalana Prabhashwara <gmchalanaprabhashwara@gmail.com>
2024-10-07 16:52:17 +0530

    Add dark mode colours for stackeholder section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primaryMain: '#030F16',
-        tileBack: '#061E2C',
+        primaryMainDark: '#030F16',
+        primaryMainLight: '#F4F5F6',
+        headerDark: '#072435',
+        headerLight: '#FFFFFF',
+        sideNavDark: '#072435',
+        sideNavLight: '#FFFFFF',
+        tileBlack: '#061E2C',
+        tileLight: '#FFFFFF',
         tileHover: '#14557C',
         navText:'#061E2C',
         darkModeBg: '#1a202c', // Custom background color for dark mode

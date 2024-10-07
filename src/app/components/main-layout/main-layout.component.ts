332e4f1883e09f4235b07b84704529981013911e
Pasan-FDo <pasanfernando.work@gmail.com>
2024-10-07 15:41:54 +0530

    responce added main layout & stackholder

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -7,7 +7,7 @@ import { NavbarComponent } from '../navbar/navbar.component';
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterModule, CommonModule, SidenavComponent, NavbarComponent ],
+  imports: [RouterModule, CommonModule, SidenavComponent, NavbarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })

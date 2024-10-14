bfd0af1c717e2c226cd0ac211275a251971dbee2
Chalana Prabhashwara <gmchalanaprabhashwara@gmail.com>
2024-10-14 15:00:50 +0530

    Fixed report sectio issue and change admin name in public forum

--- src/app/application/report-section/report/report.component.ts
+++ src/app/application/report-section/report/report.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css'
 })

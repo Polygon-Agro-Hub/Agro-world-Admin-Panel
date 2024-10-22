e4acb6bf7d1781b50ff7b4e49154cfbf462ced63
Pasan-FDo <pasanfernando.work@gmail.com>
2024-10-22 11:00:01 +0530

    marcket price dashbord

--- src/app/application/marketplace/marketplace.component.ts
+++ src/app/application/marketplace/marketplace.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-marketplace',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './marketplace.component.html',
   styleUrl: './marketplace.component.css'
 })
 export class MarketplaceComponent {
+  popupVisibleMarketPrice = false
+
+  togglePopupMarketPrice() {
+    this.popupVisibleMarketPrice = !this.popupVisibleMarketPrice
+  }
 
 }

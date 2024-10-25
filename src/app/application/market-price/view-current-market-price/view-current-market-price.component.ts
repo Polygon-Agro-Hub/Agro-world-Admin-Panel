77efe1b16a4b3d5f060174f0d9bbe077d406bde9
Pasan-FDo <pasanfernando.work@gmail.com>
2024-10-25 12:07:29 +0530

    market-price fetch function

--- src/app/application/market-price/view-current-market-price/view-current-market-price.component.ts
+++ src/app/application/market-price/view-current-market-price/view-current-market-price.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadingSpinnerComponent } from '../../../components/loading-spinner/loading-spinner.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { MarketPriceService } from '../../../services/market-price/market-price.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-current-market-price',
@@ -14,13 +16,51 @@ import { NgxPaginationModule } from 'ngx-pagination';
   templateUrl: './view-current-market-price.component.html',
   styleUrl: './view-current-market-price.component.css'
 })
-export class ViewCurrentMarketPriceComponent {
+export class ViewCurrentMarketPriceComponent implements OnInit{
   isLoading = false;
   currentDate: string;
+  market: MarketPrice[] = []
 
-  constructor() {
-    // Format the date as needed (e.g., DD/MM/YYYY)
+  constructor(private marketSrv:MarketPriceService) {
     this.currentDate = new Date().toLocaleDateString();
   }
 
+  ngOnInit(): void {
+    this.fetchAllMarketPrices()
+  }
+
+  formatDateTime(date: Date): string {
+    return new Date(date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  }
+  
+
+  fetchAllMarketPrices(){
+    this.marketSrv.getAllMarketPrice().subscribe(
+      (res)=>{
+        this.market=res
+        console.log(res);
+        
+      },
+      (error)=>{
+        console.error('Error fetching market price:', error);
+        Swal.fire(
+          'Error!',
+          'There was an error fetching market price.',
+          'error'
+        );
+      }
+    )
+  }
+
+}
+
+class MarketPrice {
+  id!: string
+  cropName!: string
+  variety!:string
+  grade!: string
+  price!: string
+  date!: string
+  startTime!: Date
+  endTime!: Date
 }

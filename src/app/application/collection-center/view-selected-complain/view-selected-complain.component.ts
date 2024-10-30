b4e30c3a4fee21054f679112b208851345fdc764
Pasan-FDo <pasanfernando.work@gmail.com>
2024-10-30 16:38:04 +0530

    select complain form done

--- src/app/application/collection-center/view-selected-complain/view-selected-complain.component.ts
+++ src/app/application/collection-center/view-selected-complain/view-selected-complain.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CollectionCenterService } from '../../../services/collection-center/collection-center.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-view-selected-complain',
   standalone: true,
   imports: [],
   templateUrl: './view-selected-complain.component.html',
-  styleUrl: './view-selected-complain.component.css'
+  styleUrl: './view-selected-complain.component.css',
+  providers: [DatePipe] // Add DatePipe to providers
 })
-export class ViewSelectedComplainComponent {
+export class ViewSelectedComplainComponent implements OnInit {
+  complain: Complain = new Complain();
+  complainId!: string;
 
+  constructor(
+    private complainSrv: CollectionCenterService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private datePipe: DatePipe 
+  ) {}
+
+  fetchComplain() {
+    this.complainSrv.getComplainById(this.complainId).subscribe((res) => {
+      res.createdAt = this.datePipe.transform(res.createdAt, 'yyyy-MM-dd hh:mm:ss a') || res.createdAt;
+      this.complain = res;
+      console.log(res);
+    });
+  }
+
+  ngOnInit(): void {
+    this.complainId = this.route.snapshot.params['id'];
+    this.fetchComplain();
+  }
+}
+
+class Complain {
+  id!: string;
+  refNo!: string;
+  status!: string;
+  officerName!: string;
+  farmerName!: string;
+  officerPhone!: string;
+  farmerPhone!: string;
+  complain!: string;
+  language!: string;
+  createdAt!: string;
 }
--- src/app/services/collection-center/collection-center.service.ts
+++ src/app/services/collection-center/collection-center.service.ts
@@ -74,4 +74,14 @@ export class CollectionCenterService {
     });
   }
 
+  getComplainById(id:string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`,
+      'Content-Type': 'application/json',
+    });
+    return this.http.get(`${this.apiUrl}get-complain-by-id/${id}`, {
+      headers,
+    });
+  }
+
 }

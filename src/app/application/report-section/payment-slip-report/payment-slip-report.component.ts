102d10d11a7acbf59a55e7e6f85dc1a8fbe54d73
bimangunarathne <bimangunarathne@outlook.com>
2024-10-30 11:34:58 +0530

    created the payment slip report ui

--- src/app/application/report-section/payment-slip-report/payment-slip-report.component.ts
+++ src/app/application/report-section/payment-slip-report/payment-slip-report.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-payment-slip-report',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CalendarModule],
   templateUrl: './payment-slip-report.component.html',
   styleUrl: './payment-slip-report.component.css'
 })

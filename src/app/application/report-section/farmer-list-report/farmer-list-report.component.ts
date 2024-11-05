abb47d57b4e57f94d02bbc53cca86c80b06824d1
bimangunarathne <bimangunarathne@outlook.com>
2024-11-05 13:33:47 +0530

    update the code

--- src/app/application/report-section/farmer-list-report/farmer-list-report.component.ts
+++ src/app/application/report-section/farmer-list-report/farmer-list-report.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-farmer-list-report',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CalendarModule],
   templateUrl: './farmer-list-report.component.html',
   styleUrl: './farmer-list-report.component.css'
 })
 export class FarmerListReportComponent {
+  todayDate!:string;
 
+  ngOnInit():void{
+    const today = new Date();
+    this.todayDate = today.toISOString().split('T')[0];
+  }
 }
---
+++ src/app/services/reports/farmer-list-report.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environment/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FarmerListReportService {
+
+  private apiUrl = `${environment.API_BASE_URL}`;
+  private token = `${environment.TOKEN}`;
+
+  constructor(private http:HttpClient) { }
+
+  getFarmerListReport(officerID: number):Observable<any>{
+    return this.http.get<any[]>(
+      `${this.apiUrl}/farmer-list-report?officerID=${officerID}`
+    )
+  }
+}

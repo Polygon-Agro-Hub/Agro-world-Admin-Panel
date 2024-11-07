e22673cd39370945ae8571cdad785355cd53a72a
Pasan-FDo <pasanfernando.work@gmail.com>
2024-11-07 13:14:46 +0530

    marcket price component replace

--- src/app/application/collection-hub/collection-hub.component.ts
+++ src/app/application/collection-hub/collection-hub.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { environment } from '../../environment/environment';
+import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-collection-hub',
@@ -11,6 +14,11 @@ import { Component } from '@angular/core';
 export class CollectionHubComponent {
   popupVisibleCollectionCenter = false;
   popupVisibleComplains = false;
+  popupVisibleMarketPrice = false
+  private token = `${environment.TOKEN}`;
+
+
+  constructor(private http: HttpClient) { }
 
   togglePopupCollectionCenter() {
     this.popupVisibleCollectionCenter = !this.popupVisibleCollectionCenter;
@@ -24,5 +32,19 @@ export class CollectionHubComponent {
     if(this.popupVisibleCollectionCenter = true){
       this.popupVisibleCollectionCenter = !this.popupVisibleCollectionCenter;
     }
+  }
+
+  togglePopupMarketPrice() {
+    this.popupVisibleMarketPrice = !this.popupVisibleMarketPrice
+  }
+
+  downloadTemplate1() {
+    const apiUrl = 'http://localhost:3000/api/market-price/download/market_price_format.xlsx';
+    window.location.href = apiUrl;
+    Swal.fire({
+      icon: 'success',
+      title: 'Downloaded',
+      text: 'Please check the download folder',
+    });
   }
 }

df69d419094d79df8fbc764c29ac22946d51d0ff
Pasan-FDo <pasanfernando.work@gmail.com>
2024-11-11 13:25:25 +0530

    2nd row dashbord

--- src/app/application/marketplace/market-place-dashbord/dashbord/market-place-dashbord.component.ts
+++ src/app/application/marketplace/market-place-dashbord/dashbord/market-place-dashbord.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { DashbordFirstRowComponent } from '../dashbord-components/dashbord-first-row/dashbord-first-row.component';
 import { DashbordAreaChartComponent } from '../dashbord-components/dashbord-area-chart/dashbord-area-chart.component';
 import { DashbordPieChartComponent } from '../dashbord-components/dashbord-pie-chart/dashbord-pie-chart.component';
+import { DashbordSecondRowComponent } from '../dashbord-components/dashbord-second-row/dashbord-second-row.component';
 
 @Component({
   selector: 'app-market-place-dashbord',
   standalone: true,
-  imports: [DashbordFirstRowComponent, DashbordAreaChartComponent, DashbordPieChartComponent],
+  imports: [DashbordFirstRowComponent, DashbordAreaChartComponent, DashbordPieChartComponent, DashbordSecondRowComponent],
   templateUrl: './market-place-dashbord.component.html',
   styleUrl: './market-place-dashbord.component.css'
 })

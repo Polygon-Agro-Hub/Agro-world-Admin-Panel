42e619c3168d05a16dc50aefffe1458bc62e7484
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2024-11-19 13:18:54 +0530

    updat the code

---
+++ src/app/environment/environment..ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    API_BASE_URL: 'http://localhost:3000/api/auth/',
+    API_URL: 'http://localhost:3000/api/',
+    TOKEN : localStorage.getItem('Login Token : ')
+  };

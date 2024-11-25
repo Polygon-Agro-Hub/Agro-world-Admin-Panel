5c2439760aa25233d128202dcd56a1eb1559bd38
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2024-11-25 08:52:01 +0530

    rename the en file

--- src/app/environment/environment..ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    API_BASE_URL: 'http://localhost:3000/api/auth/',
-    API_URL: 'http://localhost:3000/api/',
-    TOKEN : localStorage.getItem('Login Token : ')
-  };

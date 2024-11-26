fa421fe834f2c83311ea83d49c085e1ff61f2c75
bimangunarathne <bimangunarathne@outlook.com>
2024-11-26 08:57:25 +0530

    update the button

--- src/app/application/main-components/sidenav/sidenav.component.ts
+++ src/app/application/main-components/sidenav/sidenav.component.ts
@@ -77,6 +77,7 @@ export class SidenavComponent implements OnInit {
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
       }).then((result) => {
         if (result.isConfirmed) {
           // Redirect to the login page after confirmation

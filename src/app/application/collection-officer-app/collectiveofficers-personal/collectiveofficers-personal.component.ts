3b4131acd3961847d3c4bda9e2ca89ef98817d2c
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2024-12-13 13:34:33 +0530

    updat the code

--- src/app/application/collection-officer-app/collectiveofficers-personal/collectiveofficers-personal.component.ts
+++ src/app/application/collection-officer-app/collectiveofficers-personal/collectiveofficers-personal.component.ts
@@ -214,6 +214,38 @@ export class CollectiveofficersPersonalComponent implements OnInit {
   }
 
   nextForm(page: 'pageOne' | 'pageTwo') {
+    if (page === 'pageTwo') {
+      if (
+        !this.personalData.firstNameEnglish ||
+        !this.personalData.firstNameSinhala ||
+        !this.personalData.firstNameTamil ||
+        !this.personalData.lastNameEnglish ||
+        !this.personalData.lastNameSinhala ||
+        !this.personalData.lastNameTamil ||
+        !this.personalData.phoneNumber01Code ||
+        !this.personalData.lastNameEnglish ||
+        !this.personalData.phoneNumber01 ||
+        !this.personalData.phoneNumber02Code ||
+        !this.personalData.phoneNumber02 ||
+        !this.personalData.nic ||
+        !this.personalData.email ||
+        !this.personalData.houseNumber ||
+        !this.personalData.streetName ||
+        !this.personalData.city ||
+        !this.personalData.district ||
+        !this.personalData.province ||
+        !this.personalData.country ||
+        !this.personalData.languages
+
+      ) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Please fill all fields',
+          text: 'All required fields must be completed to proceed to the next page.'
+        });
+        return; // Stop navigation if validation fails
+      }
+    }
     this.selectedPage = page;
   }
 

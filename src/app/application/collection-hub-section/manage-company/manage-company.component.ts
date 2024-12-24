83f60ba07efd9f818289c5fe5fb8822218a0e0f9
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2024-12-24 08:21:30 +0530

    adde companys managemnt

--- src/app/application/collection-hub-section/manage-company/manage-company.component.ts
+++ src/app/application/collection-hub-section/manage-company/manage-company.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoadingSpinnerComponent } from "../../../components/loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-manage-company',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent],
   templateUrl: './manage-company.component.html',
   styleUrl: './manage-company.component.css'
 })

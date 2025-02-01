05cdaa503eed01680973222689ab7a0c36d06beb
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-02-01 18:31:34 +0530

    Update environment configuration for local development setup

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,13 @@
+// export const environment = {
+//   API_URL: (window["env"] as { apiUrl: string }).apiUrl || "default",
+//   API_BASE_URL:
+//     (window["env"] as { apiBaseUrl: string }).apiBaseUrl || "default",
+// };
+
 export const environment = {
-  API_URL: (window["env"] as { apiUrl: string }).apiUrl || "default",
-  API_BASE_URL:
-    (window["env"] as { apiBaseUrl: string }).apiBaseUrl || "default",
+  production: true,
+  API_BASE_URL: 'http://localhost:3000/api/auth/',
+  API_URL: 'http://localhost:3000/api/',
+  // API_BASE_URL: 'https://backend.agroworld.lk/api/auth/',
+  TOKEN: localStorage.getItem('Login Token : '),
 };

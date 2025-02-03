30fd55201c3e042b149fb95f2294b8a714c8c44b
GMchalana <gmchalanaprabhashwara@gmail.com>
2025-02-03 16:28:22 +0530

    Refactor environment configuration to use dynamic API URLs from window object

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,16 +1,5 @@
-// export const environment = {
-//     production: true,
-//     API_BASE_URL: 'https://devapi.agroworld.lk/api/auth/',
-//     API_URL: 'https://devapi.agroworld.lk/api/',
-//     // API_BASE_URL: 'https://backend.agroworld.lk/api/auth/',
-//     TOKEN : localStorage.getItem('Login Token : ')
-//   };
-
-
 export const environment = {
-    production: true,
-    API_BASE_URL: 'http://localhost:3000/api/auth/',
-    API_URL: 'http://localhost:3000/api/',
-    // API_BASE_URL: 'https://backend.agroworld.lk/api/auth/',
-    TOKEN : localStorage.getItem('Login Token : ')
-  };
+  API_URL: (window["env"] as { apiUrl: string }).apiUrl || "default",
+  API_BASE_URL:
+    (window["env"] as { apiBaseUrl: string }).apiBaseUrl || "default",
+};

8aca0932064d00eea5e006096e0fe99bd178b8a2
Pasan-FDo <pasanfernando.work@gmail.com>
2025-02-20 14:00:00 +0530

    Assert routes fixed

--- src/app/application/plant-care-app/fixed-asset-category/fixed-asset-category.component.ts
+++ src/app/application/plant-care-app/fixed-asset-category/fixed-asset-category.component.ts
@@ -17,52 +17,56 @@ export class FixedAssetCategoryComponent {
   constructor(private assetsService: AssetsService, private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
 
 
-  
+
   userId: any | null = null;
   firstName: string = '';
   lastName: string = '';
   fullName: string = '';
- 
+
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       this.userId = params['id'] ? +params['id'] : null;
       this.firstName = params['firstName'] ? params['firstName'] : null;
       this.lastName = params['lastName'] ? params['lastName'] : null;
-      
-      
+
+
       console.log('Received item ID:', this.userId);
-      console.log(this.firstName,this.lastName);
-      
+      console.log(this.firstName, this.lastName);
+
     });
 
     this.fullName = `${this.firstName} ${this.lastName}`;
-    
+
   }
 
 
   buildingCategory(id: number) {
-    this.router.navigate(['admin/plant-care/action/assets/fixed-asset-category/building-fixed-asset'], { 
-      queryParams: { id, category: 'Building and Infrastructures' , fullName: this.fullName} 
+    this.router.navigate(['/plant-care/action/assets/fixed-asset-category/building-fixed-asset'], {
+      queryParams: { id, category: 'Building and Infrastructures', fullName: this.fullName }
     });
   }
 
   landCategory(id: number) {
-    this.router.navigate(['admin/plant-care/action/assets/fixed-asset-category/land-fixed-asset'], { 
-      queryParams: { id, category: 'Land', fullName: this.fullName } 
+    this.router.navigate(['/plant-care/action/assets/fixed-asset-category/land-fixed-asset'], {
+      queryParams: { id, category: 'Land', fullName: this.fullName }
     });
   }
 
   machCategory(id: number) {
-    this.router.navigate(['admin/plant-care/action/assets/fixed-asset-category/machinary&tools-fixed-asset'], { 
-      queryParams: { id, category: 'Machinery and Vehicles', fullName: this.fullName } 
+    this.router.navigate(['/plant-care/action/assets/fixed-asset-category/machinary&tools-fixed-asset'], {
+      queryParams: { id, category: 'Machinery and Vehicles', fullName: this.fullName }
     });
   }
 
   toolsCategory(id: number) {
-    this.router.navigate(['admin/plant-care/action/assets/fixed-asset-category/machinary&tools-fixed-asset'], { 
-      queryParams: { id, category: 'Tools and Equipments', fullName: this.fullName } 
+    this.router.navigate(['/plant-care/action/assets/fixed-asset-category/machinary&tools-fixed-asset'], {
+      queryParams: { id, category: 'Tools and Equipments', fullName: this.fullName }
     });
+  }
+
+  navigatePath(path: string) {
+    this.router.navigate([path])
   }
 
 }

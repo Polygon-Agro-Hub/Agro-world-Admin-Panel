b3cecbc4ba1ef307906c7bd6606fd9771ff83117
GMchalana <gmchalanaprabhashwara@gmail.com>
2025-02-21 18:19:46 +0530

    change environment

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,29 +1,4 @@
-// export const environment = {
-//   production: true,
-//   API_BASE_URL: 'https://devapi.agroworld.lk/api/auth/',
-//   API_URL: 'https://devapi.agroworld.lk/api/',
-//   // API_BASE_URL: 'https://backend.agroworld.lk/api/auth/',
-//   TOKEN: localStorage.getItem('Login Token : '),
-// };
-
-// export const environment = {
-//   production: true,
-//   API_BASE_URL: 'http://localhost:3000/api/auth/',
-//   API_URL: 'http://localhost:3000/api/',
-//   // API_BASE_URL: 'https://backend.agroworld.lk/api/auth/',
-//   TOKEN: localStorage.getItem('Login Token : '),
-// };
-
-// export const environment = {
-//   production: true,
-//   API_BASE_URL: 'http://localhost:3000/api/auth/',
-//   API_URL: 'http://localhost:3000/api/',
-//   // API_BASE_URL: 'https://backend.agroworld.lk/api/auth/',
-//   TOKEN: localStorage.getItem('Login Token : '),
-// };
-
 export const environment = {
   production: true,
-
   API_URL: 'http://localhost:3000/agro-api/admin-api/api/',
 };

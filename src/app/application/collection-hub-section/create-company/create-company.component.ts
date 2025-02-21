e658e50546dc06f91461c9a29e09feae656894c3
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-02-21 11:10:18 +0530

    Refactor CreateCompanyComponent for improved readability and maintainability

--- src/app/application/collection-hub-section/create-company/create-company.component.ts
+++ src/app/application/collection-hub-section/create-company/create-company.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { CollectionCenterService } from '../../../services/collection-center/collection-center.service';
@@ -15,7 +21,7 @@ import { error } from 'console';
   standalone: true,
   imports: [ReactiveFormsModule, HttpClientModule, CommonModule, FormsModule],
   templateUrl: './create-company.component.html',
-  styleUrl: './create-company.component.css'
+  styleUrl: './create-company.component.css',
 })
 export class CreateCompanyComponent {
   companyData: Company = new Company();
@@ -24,13 +30,12 @@ export class CreateCompanyComponent {
   itemId: number | null = null;
   touchedFields: { [key in keyof Company]?: boolean } = {};
 
-
   constructor(
     private fb: FormBuilder,
     private collectionCenterSrv: CollectionCenterService,
     private route: ActivatedRoute,
     private http: HttpClient,
-    private router: Router,
+    private router: Router
   ) {
     this.userForm = this.fb.group({
       regNumber: ['', Validators.required],
@@ -51,135 +56,137 @@ export class CreateCompanyComponent {
       foName: ['', Validators.required],
       foConCode: ['', Validators.required],
       foConNum: ['', Validators.required],
-      foEmail: ['', [Validators.required]]
+      foEmail: ['', [Validators.required]],
     });
   }
 
-
   ngOnInit() {
     // Subscribe to form value changes
-    this.route.queryParams.subscribe((params)=>{
+    this.route.queryParams.subscribe((params) => {
       this.itemId = params['id'] ? +params['id'] : null;
       console.log('Received item ID:', this.itemId);
     });
-    this.userForm.valueChanges.subscribe(formValues => {
+    this.userForm.valueChanges.subscribe((formValues) => {
       this.companyData = { ...this.companyData, ...formValues };
     });
     this.getCompanyData();
   }
 
-  
-getCompanyData() {
-  if (this.itemId) {
-    this.collectionCenterSrv.getCompanyById(this.itemId).subscribe(
-      (response: any) => {
-        console.log('Fetched company data:', response);
+  getCompanyData() {
+    if (this.itemId) {
+      this.collectionCenterSrv.getCompanyById(this.itemId).subscribe(
+        (response: any) => {
+          console.log('Fetched company data:', response);
 
-        this.companyData = response;
-        console.log("--check--",this.companyData);
-      },
-      (error) => {
-        console.error('Error fetching company data:', error);
-        Swal.fire('Error', 'Failed to fetch company data. Please try again.', 'error');
-      }
-    );
+          this.companyData = response;
+          console.log('--check--', this.companyData);
+        },
+        (error) => {
+          console.error('Error fetching company data:', error);
+          Swal.fire(
+            'Error',
+            'Failed to fetch company data. Please try again.',
+            'error'
+          );
+        }
+      );
+    }
   }
-}
-
 
   saveCompanyData() {
     console.log(this.companyData);
-      
-      this.collectionCenterSrv.createCompany(this.companyData).subscribe(
-        (response) => {
-          console.log('Data saved successfully:', response);
-          Swal.fire('Success', 'Company Created Successfully', 'success');
-          this.router.navigate(['/collection-hub/manage-company']);
-        },
-        (error) => {
-          console.error('Error saving data:', error);
-          Swal.fire('Error', error, 'error');
-        }
-      );
-   
+
+    this.collectionCenterSrv.createCompany(this.companyData).subscribe(
+      (response) => {
+        console.log('Data saved successfully:', response);
+        Swal.fire('Success', 'Company Created Successfully', 'success');
+        this.router.navigate(['/collection-hub/manage-company']);
+      },
+      (error) => {
+        console.error('Error saving data:', error);
+        Swal.fire('Error', error, 'error');
+      }
+    );
   }
 
-
-
-
   nextFormCreate(page: 'pageOne' | 'pageTwo') {
-      if (page === 'pageTwo') {
-        
-        const missingFields: string[] = [];
-  
-        if (!this.companyData.regNumber) missingFields.push('Company Register Number');
-        if (!this.companyData.companyNameEnglish) missingFields.push('Company Name (English)');
-        if (!this.companyData.companyNameSinhala) missingFields.push('Company Name (Sinhala)');
-        if (!this.companyData.companyNameTamil) missingFields.push('Company Name (Tamil)');
-        if (!this.companyData.email) missingFields.push('Company Email');
-        // if (!this.companyData.oicName) missingFields.push('Officer In Charge Name');
-        // if (!this.companyData.oicEmail) missingFields.push('Officer In Charge Email');
-        // if (!this.companyData.oicConCode1) missingFields.push('Phone Number 01 Code');
-        // if (!this.companyData.oicConNum1) missingFields.push('Phone Number 02');
+    if (page === 'pageTwo') {
+      const missingFields: string[] = [];
 
-    
-        if (missingFields.length > 0) {
-          Swal.fire({
-            icon: 'error',
-            title: 'Please fill all fields',
-            html: `The following fields are missing:<br><ul>${missingFields
-              .map(field => `<li>${field}</li>`)
-              .join('')}</ul>`
-          });
-          return;
-        }
+      if (!this.companyData.regNumber)
+        missingFields.push('Company Register Number');
+      if (!this.companyData.companyNameEnglish)
+        missingFields.push('Company Name (English)');
+      if (!this.companyData.companyNameSinhala)
+        missingFields.push('Company Name (Sinhala)');
+      if (!this.companyData.companyNameTamil)
+        missingFields.push('Company Name (Tamil)');
+      if (!this.companyData.email) missingFields.push('Company Email');
+      // if (!this.companyData.oicName) missingFields.push('Officer In Charge Name');
+      // if (!this.companyData.oicEmail) missingFields.push('Officer In Charge Email');
+      // if (!this.companyData.oicConCode1) missingFields.push('Phone Number 01 Code');
+      // if (!this.companyData.oicConNum1) missingFields.push('Phone Number 02');
+
+      if (missingFields.length > 0) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Please fill all fields',
+          html: `The following fields are missing:<br><ul>${missingFields
+            .map((field) => `<li>${field}</li>`)
+            .join('')}</ul>`,
+        });
+        return;
       }
-  
-      this.selectedPage = page;
     }
 
-    updateCompanyData(){
-      if(this.itemId){
-        this.collectionCenterSrv.updateCompany(this.companyData, this.itemId).subscribe(
-          (response)=>{
+    this.selectedPage = page;
+  }
+
+  updateCompanyData() {
+    if (this.itemId) {
+      this.collectionCenterSrv
+        .updateCompany(this.companyData, this.itemId)
+        .subscribe(
+          (response) => {
             console.log('Company updated successfully:', response);
             Swal.fire('Success', 'Company Updated Successfully', 'success');
             this.router.navigate(['/collection-hub/manage-company']);
           },
-          (error) =>{
+          (error) => {
             console.error('Error updating company:', error);
-            Swal.fire('Error', 'Failed to update company. Please try again.', 'error');
+            Swal.fire(
+              'Error',
+              'Failed to update company. Please try again.',
+              'error'
+            );
           }
         );
-      }else{
-        Swal.fire('Error', 'No company ID found for update', 'error');
-      }
+    } else {
+      Swal.fire('Error', 'No company ID found for update', 'error');
     }
+  }
 
+  onBlur(fieldName: keyof Company): void {
+    this.touchedFields[fieldName] = true;
+  }
 
+  isFieldInvalid(fieldName: keyof Company): boolean {
+    return !!this.touchedFields[fieldName] && !this.companyData[fieldName];
+  }
 
+  isValidEmail(email: string): boolean {
+    const emailPattern =
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|net|org|lk|co)$/i;
+    return emailPattern.test(email);
+  }
 
-
-    onBlur(fieldName: keyof Company): void {
-      this.touchedFields[fieldName] = true;
-    }
-  
-    isFieldInvalid(fieldName: keyof Company): boolean {
-      return !!this.touchedFields[fieldName] && !this.companyData[fieldName];
-    }
-
-    back(): void {
-      this.router.navigate(['/collection-hub']);
-    }
-    
-
-
-
+  back(): void {
+    this.router.navigate(['/collection-hub']);
+  }
 }
 
-
 class Company {
-  id!:number;
+  id!: number;
   regNumber!: string;
   companyNameEnglish!: string;
   companyNameSinhala!: string;

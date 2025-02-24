8f86360f74576944b672c2f2ec7b181fe76298bf
bimangunarathne <bimangunarathne@outlook.com>
2025-02-24 18:47:41 +0530

    Add customer details popup with copy to clipboard functionality

--- src/app/application/sales-dash-section/customers/customers.component.ts
+++ src/app/application/sales-dash-section/customers/customers.component.ts
@@ -5,6 +5,8 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { response } from 'express';
 import { LoadingSpinnerComponent } from '../../../components/loading-spinner/loading-spinner.component';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 interface Customers {
   id: number;
@@ -15,21 +17,35 @@ interface Customers {
   phoneNumber: string;
   email: string;
   buildingType: string;
-  salesAgent: number;
+  salesAgentEmpId: number;
+  salesAgentFirstName: string;
+  salesAgentLastName: string;
   created_at: string;
+  houseHouseNo: string;
+  houseStreetName: string;
+  houseCity: string;
+  apartmentBuildingNo: string;
+  apartmentBuildingName: string;
+  apartmentUnitNo: string;
+  apartmentFloorNo: string;
+  apartmentHouseNo: string;
+  apartmentStreetName: string;
+  apartmentCity: string;
 }
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [CommonModule,LoadingSpinnerComponent],
+  imports: [CommonModule, LoadingSpinnerComponent, FormsModule],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css',
 })
 export class CustomersComponent {
   customers: Customers[] = [];
   isLoading = true;
-  hasData: boolean = true; 
+  hasData: boolean = true;
+  isPopupOpen = false;
+  selectedCustomer: any = null;
 
   constructor(
     private customerService: CustomersService,
@@ -39,6 +55,40 @@ export class CustomersComponent {
 
   ngOnInit() {
     this.fetchAllCustomers();
+  }
+
+  openPopup(customer: any) {
+    this.selectedCustomer = customer;
+    this.isPopupOpen = true;
+  }
+
+  closePopup() {
+    this.isPopupOpen = false;
+    this.selectedCustomer = null;
+  }
+
+  copyToClipboard(value: string | undefined) {
+    if (!value) return; // Prevent copying if value is undefined
+
+    navigator.clipboard
+      .writeText(value)
+      .then(() => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Copied!',
+          text: `${value} has been copied to clipboard.`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      })
+      .catch((err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops!',
+          text: 'Failed to copy. Please try again.',
+        });
+        console.error('Failed to copy:', err);
+      });
   }
 
   fetchAllCustomers() {

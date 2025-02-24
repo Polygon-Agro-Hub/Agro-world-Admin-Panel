02e2c87b509a1a88da8f1757520466d053a9597a
AshanIsuranga <21it0478@itum.mrt.ac.lk>
2025-02-24 19:18:35 +0530

    get all agents half completed

---
+++ src/app/services/sales-dash/sales-dash.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environment/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TokenService } from '../token/services/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalesDashService {
+
+  private apiUrl = `${environment.API_URL}`;
+  private token = this.tokenService.getToken();
+
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
+
+  getAllSalesAgents(page: number = 1, limit: number = 10,  searchText: string = '', status: string = ''): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`,
+      'Content-Type': 'application/json',
+    });
+
+    let url = `${this.apiUrl}sales-agent-dash/get-all-sales-agents?page=${page}&limit=${limit}`;
+
+
+
+    if (status) {
+      url += `&status=${status}`
+    }
+
+    if (searchText) {
+      url += `&searchText=${searchText}`
+    }
+
+    return this.http.get(url, {
+      headers,
+    });
+  }
+}

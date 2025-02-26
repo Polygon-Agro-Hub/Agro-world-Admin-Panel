528fe43e95a077b37399b4fd7af4c20a17f06b31
Pasan-FDo <pasanfernando.work@gmail.com>
2025-02-26 17:09:03 +0530

    first row of stackholder dashbord

--- src/app/application/steckholders-section/stackholder-dashboard/dashboard-main/dashboard-main.component.ts
+++ src/app/application/steckholders-section/stackholder-dashboard/dashboard-main/dashboard-main.component.ts
@@ -21,93 +21,32 @@ import { SalesAgentsRowComponent } from '../sales-agents-row/sales-agents-row.co
   styleUrl: './dashboard-main.component.css'
 })
 export class DashboardMainComponent implements OnInit {
-
-  //admin data
-  // associateAdmins!: number;
-  // executiveAdmins!: number;
-  // managerAdmins!: number;
-  // officerAdmins!: number;
-  // newAdminUsers!: number;
-  // allAdminUsers!: number;
-
-  //collection officer data
-
-  // centerHeadOfficers!: number;
-  // centerManagers!: number;
-  // collectionOfficers!: number;
-  // customerOfficers!: number;
-  // newOfficers!: number;
-  // allOfficers!: number;
-
+  firstRow: any = {};
   constructor(
     private router: Router,
-    private http: HttpClient,
-    private tokenService: TokenService,
     private stakeholderSrv: StakeholderService
-    
-    
   ) { }
 
   ngOnInit(): void {
-
-    // this.fetchAdminUserData();
-    // this.fetchCollectionOfficerData();
-
+    this.fetchAdminUserData();
   }
 
-  // fetchAdminUserData() {
 
-
-  //   console.log("fetching started");
-  //   // this.isLoading = true;
-  //   this.stakeholderSrv.getAdminUserData().subscribe(
-      
-  //     (res) => {
-  //       console.log('dtgsgdgdg',res);
-  //       this.associateAdmins = res.adminUsersByPosition[0]?.adminUserCount ?? 0;
-  //       this.executiveAdmins = res.adminUsersByPosition[1]?.adminUserCount ?? 0;
-  //       this.managerAdmins = res.adminUsersByPosition[2]?.adminUserCount ?? 0;
-  //       this.officerAdmins = res.adminUsersByPosition[3]?.adminUserCount ?? 0;
-  //       // this.adminUsersByPosition = res.adminUsersByPosition;
-  //       this.newAdminUsers = res.newAdminUsers[0]?.TotalAdminUserCount ?? 0;
-  //       this.allAdminUsers = res.allAdminUsers[0]?.TotalAdminUserCount ?? 0;
-
-
-  //     },
-  //     (error) => {
-  //       console.log("Error: ", error);
-  //       // this.isLoading = false;
-  //     }
-  //   );
-  // }
-
-
-  // fetchCollectionOfficerData() {
-
-
-  //   console.log("fetching started");
-  //   // this.isLoading = true;
-  //   this.stakeholderSrv.getCollectionOfficerData().subscribe(
-      
-  //     (res) => {
-  //       console.log('dtgsgdgdg',res);
-  //       this.centerHeadOfficers = res.collectionOfficersByPosition[0]?.officerCount ?? 0;
-  //       this.centerManagers = res.collectionOfficersByPosition[1]?.officerCount ?? 0;
-  //       this.collectionOfficers = res.collectionOfficersByPosition[2]?.officerCount ?? 0;
-  //       this.customerOfficers = res.collectionOfficersByPosition[3]?.officerCount ?? 0;
-  //       // this.adminUsersByPosition = res.adminUsersByPosition;
-  //       this.newOfficers = res.newCollectionOfficers[0]?.newOfficerCount ?? 0;
-  //       this.allOfficers = res.allCollectionOfficers[0]?.totalOfficerCount ?? 0;
-
-        
-  //     },
-  //     (error) => {
-  //       console.log("Error: ", error);
-  //       // this.isLoading = false;
-  //     }
-  //   );
-  // }
+  fetchAdminUserData() {
+    console.log("fetching started");
+    this.stakeholderSrv.getAdminUserData().subscribe(
+      (res) => {
+        console.log('Admin ->', res);
+        this.firstRow = res.firstRow
+      },
+      (error) => {
+        console.log("Error: ", error);
+      }
+    );
+  }
 
 }
+
+
 
 

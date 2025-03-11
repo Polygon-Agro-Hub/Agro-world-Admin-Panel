4b933bcef60f44a4f5d41b9f1f1636d7007be550
bimangunarathne <bimangunarathne@outlook.com>
2025-03-11 14:26:33 +0530

    Update officer target routing and data fetching logic: include officerId in route parameters and streamline data retrieval process

--- src/app/application/steckholders-section/view-officer-target/view-officer-target.component.ts
+++ src/app/application/steckholders-section/view-officer-target/view-officer-target.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { OfficerTargetService } from '../../../services/collection-officer/officer-target.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
+import { LoadingSpinnerComponent } from '../../../components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-view-officer-target',
   standalone: true,
-  imports: [CommonModule, FormsModule, DropdownModule],
+  imports: [CommonModule, FormsModule, DropdownModule, LoadingSpinnerComponent],
   templateUrl: './view-officer-target.component.html',
   styleUrl: './view-officer-target.component.css',
 })
@@ -20,55 +21,104 @@ export class ViewOfficerTargetComponent {
   hasData: boolean = true;
   selectStatus: string = '';
   searchText: string = '';
+  isLoading = false;
+
   constructor(
     private TargetSrv: OfficerTargetService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private location: Location
   ) {}
+
+  goBack() {
+    this.location.back();
+  }
 
   ngOnInit(): void{
     this.officerId = this.route.snapshot.params['officerId'];
     this.fetchSelectedOfficerTarget(this.officerId);
   }
 
-  fetchSelectedOfficerTarget(
-    officerId: number,
-    status: string = this.selectStatus,
-    search: string = this.searchText
-  ) {
-    this.TargetSrv.getSelectedOfficerTargetData(
-      officerId,
-      status,
-      search
-    ).subscribe((res) => {
-      console.log(res);
+  // fetchSelectedOfficerTarget(
+  //   officerId: number,
+  //   status: string = this.selectStatus,
+  //   search: string = this.searchText
+  // ) {
+  //   this.TargetSrv.getSelectedOfficerTargetData(
+  //     officerId,
+  //     status,
+  //     search
+  //   ).subscribe((res) => {
+  //     console.log(res);
 
-      this.selectedOfficerDataArr = res.items;
-      console.log(res.items.length);
-      if (res.items.length === 0) {
-        this.hasData = false;
-      } else {
-        this.hasData = true;
-      }
-    });
-  }
+  //     this.selectedOfficerDataArr = res.items;
+  //     console.log(res.items.length);
+  //     if (res.items.length === 0) {
+  //       this.hasData = false;
+  //     } else {
+  //       this.hasData = true;
+  //     }
+  //   });
+  // }
+
+  fetchSelectedOfficerTarget(officerId: number): void {
+    this.isLoading = true;
+    this.TargetSrv.getSelectedOfficerTargetData(officerId).subscribe(
+        (res) => {
+          this.isLoading = false;
+            console.log(res);
+
+            this.selectedOfficerDataArr = res.items;
+            console.log(res.items.length);
+
+            // Check if there is data
+            if (res.items.length === 0) {
+                this.hasData = false;
+            } else {
+                this.hasData = true;
+            }
+        },
+        (error) => {
+            console.error("Error fetching officer target data:", error);
+            this.hasData = false; // Assume no data in case of an error
+        }
+    );
+}
+
+  // cancelStatus() {
+  //   this.selectStatus = '';
+  //   this.fetchSelectedOfficerTarget(this.officerId, this.selectStatus, this.searchText);
+  // }
 
   cancelStatus() {
     this.selectStatus = '';
-    this.fetchSelectedOfficerTarget(this.officerId, this.selectStatus, this.searchText);
+    this.fetchSelectedOfficerTarget(this.officerId);
   }
+
+  // filterStatus() {
+  //   this.fetchSelectedOfficerTarget(this.officerId, this.selectStatus, this.searchText);
+  // }
 
   filterStatus() {
-    this.fetchSelectedOfficerTarget(this.officerId, this.selectStatus, this.searchText);
+    this.fetchSelectedOfficerTarget(this.officerId);
   }
 
+  // onSearch() {
+  //   this.fetchSelectedOfficerTarget(this.officerId, this.selectStatus, this.searchText);
+  // }
+
   onSearch() {
-    this.fetchSelectedOfficerTarget(this.officerId, this.selectStatus, this.searchText);
+    this.fetchSelectedOfficerTarget(this.officerId);
   }
+
+  // offSearch() {
+  //   this.searchText = '';
+  //   this.fetchSelectedOfficerTarget(this.officerId, this.selectStatus, this.searchText);
+  // }
 
   offSearch() {
     this.searchText = '';
-    this.fetchSelectedOfficerTarget(this.officerId, this.selectStatus, this.searchText);
+    this.fetchSelectedOfficerTarget(this.officerId);
   }
 }
 

5789904655ec63faae2e27e891d545719c35c3a0
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-08-06 12:29:40 +0530

    update the code

---
+++ src/app/application/collection-hub-section/center-collection-expence/center-collection-expence.component.ts
@@ -0,0 +1,263 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CollectionCenterService } from '../../../services/collection-center/collection-center.service';
+import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { LoadingSpinnerComponent } from '../../../components/loading-spinner/loading-spinner.component';
+
+@Component({
+  selector: 'app-center-collection-expence',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgxPaginationModule, LoadingSpinnerComponent],
+  templateUrl: './center-collection-expence.component.html',
+  styleUrl: './center-collection-expence.component.css'
+})
+export class CenterCollectionExpenceComponent implements OnInit {
+
+  farmerPaymentsArr!: FarmerPayments[];
+  centerArr: Center[] = [];
+  totalPaymentsAmount: number = 0; // New variable to store the sum of all payments
+
+  centerId!: number;
+
+  page: number = 1;
+  totalItems: number = 0;
+  itemsPerPage: number = 10;
+  hasData: boolean = true;
+
+  searchText: string = '';
+
+  fromDate: Date | string = '';
+  toDate: Date | string = '';
+
+  isPopupVisible: boolean = false;
+  logingRole: string | null = null;
+  isLoading: boolean = false;
+  isDateFilterSet: boolean = false;
+
+  isDownloading = false;
+
+  constructor(
+    private router: Router,
+    private TargetSrv: CollectionCenterService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.centerId = this.route.snapshot.params['id'];
+  }
+
+  navigate(path: string) {
+    this.router.navigate([`${path}`]);
+  }
+
+  fetchFilteredPayments(page: number = 1, limit: number = this.itemsPerPage) {
+    this.isLoading = true;
+
+    this.TargetSrv.getAllCenterPayments(
+      page,
+      limit,
+      this.fromDate,
+      this.toDate,
+      this.centerId,
+      this.searchText
+    ).subscribe(
+      (res) => {
+        this.farmerPaymentsArr = res.items;
+        this.totalItems = res.total;
+        this.hasData = res.items.length > 0;
+        this.isLoading = false;
+        this.calculateTotalPayments();
+      },
+      (error) => {
+        this.isLoading = false;
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Failed to fetch payments data',
+          customClass: {
+            popup: 'bg-white dark:bg-[#363636] text-gray-800 dark:text-white',
+            title: 'dark:text-white',
+          }
+        });
+      }
+    );
+  }
+
+  private formatNumberToTwoDecimals(value: any): number {
+    const num = typeof value === 'string' ? parseFloat(value) : Number(value);
+    return parseFloat(num.toFixed(2));
+  }
+
+  calculateTotalPayments(): void {
+    this.totalPaymentsAmount = this.farmerPaymentsArr.reduce((sum, payment) => {
+      const amount = this.formatNumberToTwoDecimals(payment.totalAmount);
+      return sum + (isNaN(amount) ? 0 : amount);
+    }, 0);
+
+    this.totalPaymentsAmount = this.formatNumberToTwoDecimals(this.totalPaymentsAmount);
+  }
+
+  onSearch() {
+    this.fetchFilteredPayments();
+  }
+
+  offSearch() {
+    this.searchText = '';
+    this.fetchFilteredPayments();
+  }
+
+  onPageChange(event: number) {
+    this.page = event;
+    this.fetchFilteredPayments(this.page, this.itemsPerPage);
+  }
+
+  validateToDate() {
+    if (!this.fromDate) {
+      this.toDate = '';
+      Swal.fire({
+        icon: 'warning',
+        title: 'Warning',
+        text: "Please select the 'From' date first",
+        customClass: {
+          popup: 'bg-white dark:bg-[#363636] text-gray-800 dark:text-white',
+          title: 'dark:text-white',
+        }
+      });
+      return;
+    }
+
+    if (this.toDate) {
+      const from = new Date(this.fromDate);
+      const to = new Date(this.toDate);
+
+      if (to <= from) {
+        this.toDate = '';
+        Swal.fire({
+          icon: 'warning',
+          title: 'Warning',
+          text: "The 'To' date cannot be earlier than or same to the 'From' date",
+          customClass: {
+            popup: 'bg-white dark:bg-[#363636] text-gray-800 dark:text-white',
+            title: 'dark:text-white',
+          }
+        });
+      }
+    }
+  }
+
+  validateFromDate() {
+    if (!this.toDate) {
+      return;
+    }
+
+    if (this.toDate) {
+      const from = new Date(this.fromDate);
+      const to = new Date(this.toDate);
+
+      if (to <= from) {
+        this.fromDate = '';
+        Swal.fire({
+          icon: 'warning',
+          title: 'Warning',
+          text: "The 'From' date cannot be later than or same to the 'To' date",
+          customClass: {
+            popup: 'bg-white dark:bg-[#363636] text-gray-800 dark:text-white',
+            title: 'dark:text-white',
+          }
+        });
+      }
+    }
+  }
+
+  goBtn() {
+    if (!this.fromDate || !this.toDate) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Warning',
+        text: 'Please fill in all date fields',
+        customClass: {
+          popup: 'bg-white dark:bg-[#363636] text-gray-800 dark:text-white',
+          title: 'dark:text-white',
+        }
+      });
+      return;
+    }
+    this.isDateFilterSet = true;
+    this.fetchFilteredPayments();
+  }
+
+  downloadTemplate1() {
+    this.isDownloading = true;
+
+    this.TargetSrv
+      .downloadCenterPaymentReportFile(this.fromDate, this.toDate, this.centerId, this.searchText)
+      .subscribe({
+        next: (blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = `Expenses Report From ${this.fromDate} To ${this.toDate}.xlsx`;
+          a.click();
+          window.URL.revokeObjectURL(url);
+
+          Swal.fire({
+            icon: "success",
+            title: "Downloaded",
+            text: "Please check your downloads folder",
+            customClass: {
+              popup: 'bg-white dark:bg-[#363636] text-gray-800 dark:text-white',
+              title: 'dark:text-white',
+            }
+          });
+          this.isDownloading = false;
+        },
+        error: (error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Download Failed",
+            text: error.message,
+            customClass: {
+              popup: 'bg-white dark:bg-[#363636] text-gray-800 dark:text-white',
+              title: 'dark:text-white',
+            }
+          });
+          this.isDownloading = false;
+        }
+      });
+  }
+
+  navigateToFarmerReport(invNo: string) {
+    this.router.navigate([`reports/farmer-report-invoice/${invNo}`]);
+  }
+
+  navigateToCenterTarget() {
+    this.router.navigate(['/centers']);
+  }
+}
+
+class FarmerPayments {
+  invNo!: string;
+  totalAmount!: number;
+  centerCode!: string;
+  centerName!: string;
+  nic!: string;
+  firstNameEnglish!: string;
+  gradeAprice!: number;
+  gradeBprice!: number;
+  gradeCprice!: number;
+  gradeAquan!: number;
+  gradeBquan!: number;
+  gradeCquan!: number;
+  status!: string;
+  createdAt!: string | Date;
+  companyId!: number;
+}
+
+class Center {
+  id!: number;
+  centerName!: string;
+  regCode!: string;
+}

8418ce095a233c149ebb13bd525584e83c73f4ac
bimangunarathne <bimangunarathne@outlook.com>
2025-08-27 14:48:35 +0530

    update the code

--- src/app/application/distribution-hub/polygon-centers-dashboard/officer-target/officer-target.component.ts
+++ src/app/application/distribution-hub/polygon-centers-dashboard/officer-target/officer-target.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LoadingSpinnerComponent } from '../../../../components/loading-spinner/loading-spinner.component';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-officer-target',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    LoadingSpinnerComponent,
+    CalendarModule,
+    DropdownModule,
+    NgxPaginationModule,
+  ],
   templateUrl: './officer-target.component.html',
-  styleUrl: './officer-target.component.css'
+  styleUrl: './officer-target.component.css',
 })
 export class OfficerTargetComponent {
+  isLoading = false;
+  hasData: boolean = false;
 
+  ngOnInit(): void {}
 }

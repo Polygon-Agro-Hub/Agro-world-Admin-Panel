8e313054c37e7be9620f5e70f7a4952d688c8941
bimangunarathne <bimangunarathne@outlook.com>
2025-08-27 13:08:04 +0530

    update the code

--- src/app/application/distribution-hub/polygon-centers-dashboard/out-of-delivery/out-of-delivery.component.ts
+++ src/app/application/distribution-hub/polygon-centers-dashboard/out-of-delivery/out-of-delivery.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { LoadingSpinnerComponent } from '../../../../components/loading-spinner/loading-spinner.component';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-out-of-delivery',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    LoadingSpinnerComponent,
+    CalendarModule,
+    DropdownModule,
+    NgxPaginationModule,
+  ],
   templateUrl: './out-of-delivery.component.html',
-  styleUrl: './out-of-delivery.component.css'
+  styleUrl: './out-of-delivery.component.css',
 })
-export class OutOfDeliveryComponent {
+export class OutOfDeliveryComponent implements OnInit {
+  isLoading = false;
+  hasData: boolean = false;
 
+  ngOnInit(): void {}
 }

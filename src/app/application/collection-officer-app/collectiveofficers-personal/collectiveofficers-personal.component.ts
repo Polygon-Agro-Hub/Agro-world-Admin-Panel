3aab35d403037fa6a03865d7e22e0c08f7f14ba0
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-09-16 12:39:05 +0530

    update the code

--- src/app/application/collection-officer-app/collectiveofficers-personal/collectiveofficers-personal.component.ts
+++ src/app/application/collection-officer-app/collectiveofficers-personal/collectiveofficers-personal.component.ts
@@ -98,8 +98,6 @@ export class CollectiveofficersPersonalComponent implements OnInit {
   { code: 'BD', dialCode: '+880', name: 'Bangladesh' },
   { code: 'IN', dialCode: '+91', name: 'India' },
   { code: 'NL', dialCode: '+31', name: 'Netherlands' },
-  { code: 'UK', dialCode: '+44', name: 'United Kingdom' },
-  { code: 'US', dialCode: '+1', name: 'United States' }
 ];
 
 getFlagUrl(countryCode: string): string {

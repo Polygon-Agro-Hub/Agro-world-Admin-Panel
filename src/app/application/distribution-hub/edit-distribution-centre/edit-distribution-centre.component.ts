57c5596aa2af9f67ca0e129c4b1ec5f455c35c53
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-09-19 17:07:57 +0530

    update the code

--- src/app/application/distribution-hub/edit-distribution-centre/edit-distribution-centre.component.ts
+++ src/app/application/distribution-hub/edit-distribution-centre/edit-distribution-centre.component.ts
@@ -891,7 +891,7 @@ back(): void {
                   popup: 'bg-tileLight dark:bg-tileBlack text-black dark:text-white',
                 }
               }).then(() => {
-                this.router.navigate(['/distribution-hub/action/view-destribition-center']);
+                this.router.navigate(['/distribution-hub/action/view-polygon-centers']);
               });
             } else {
               this.showErrorAlert(response.error || 'Update failed');

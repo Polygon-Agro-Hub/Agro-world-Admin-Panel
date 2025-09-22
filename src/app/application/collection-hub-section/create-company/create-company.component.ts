f076b21a770063c5a99361f528630a78f7313e17
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-09-22 11:08:01 +0530

    update the code

--- src/app/application/collection-hub-section/create-company/create-company.component.ts
+++ src/app/application/collection-hub-section/create-company/create-company.component.ts
@@ -49,7 +49,7 @@ interface BranchesData {
 export class CreateCompanyComponent implements OnInit {
   companyData: Company = new Company();
   userForm: FormGroup;
-  selectedPage: 'pageOne' | 'pageTwo' = 'pageTwo';
+  selectedPage: 'pageOne' | 'pageTwo' = 'pageOne';
   itemId: number | null = null;
   touchedFields: { [key in keyof Company]?: boolean } = {};
   banks: Bank[] = [];

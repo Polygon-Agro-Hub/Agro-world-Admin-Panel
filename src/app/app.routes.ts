69a95d6426f9d0f3a29bf6521e2ed7b7b42a4e08
Avishka777 <a.rathnakumara777@gmail.com>
2025-09-30 17:19:42 +0530

    Develop Create and VIew Certificate Company Details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -187,6 +187,8 @@ import { FinanceDashboardComponent } from './application/finance/finance-dashboa
 import path from 'path';
 import { ResetPasswordComponent } from './application/main-components/reset-password/reset-password.component';
 import { GoviLinkDashbordComponent } from './application/govi-link/govi-link-dashbord/govi-link-dashbord.component';
+import { AddCompanyDetailsComponent } from './application/plant-care-app/add-company-details/add-company-details';
+import { ViewCompanyListComponent } from './application/plant-care-app/view-company-list/view-company-list.component';
 export const routes: Routes = [
   {
     path: '',
@@ -418,6 +420,18 @@ export const routes: Routes = [
                     data: { permission: 'Manage Opt-Out feedback list' },
                   },
                 ],
+              },
+              {
+                path: 'add-company-details',
+                component: AddCompanyDetailsComponent,
+                canActivate: [PermissionGuard],
+                data: { permission: 'Create company details' },
+              },
+              {
+                path: 'view-company-list',
+                component: ViewCompanyListComponent,
+                canActivate: [PermissionGuard],
+                data: { permission: 'View company details' },
               },
             ],
           },

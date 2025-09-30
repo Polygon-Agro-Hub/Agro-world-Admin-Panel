4f6126673cb48e050417db2469fba431f8fadedf
Pasan-FDo <pasanfernando.work@gmail.com>
2025-09-30 10:03:32 +0530

    Refactor GoviLinkService to include API URL and token management

--- src/app/services/govi-link/govi-link.service.ts
+++ src/app/services/govi-link/govi-link.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environment/environment.development';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TokenService } from '../token/services/token.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoviLinkService {
+  private apiUrl = `${environment.API_URL}govi-link/`;
+  private token = this.tokenService.getToken();
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService,
+  ) { }
 
-  constructor() { }
+  // getAllSystemApplications(): Observable<any> {
+  //   const headers = new HttpHeaders({
+  //     Authorization: `Bearer ${this.token}`,
+  //     "Content-Type": "application/json",
+  //   });
+  //   return this.http.get(`${this.apiUrl}complain/get-all-system-applications`, {
+  //     headers,
+  //   });
+  // }
 }

be12ec43773830ab79d667be7ee5e59979fa0bc9
AshanIsuranga <21it0478@itum.mrt.ac.lk>
2025-10-03 09:25:57 +0530

    bug fix

--- src/app/application/collection-hub-section/create-company/create-company.component.ts
+++ src/app/application/collection-hub-section/create-company/create-company.component.ts
@@ -275,23 +275,27 @@ export class CreateCompanyComponent implements OnInit {
   }
 
   back(): void {
-    Swal.fire({
-      icon: 'warning',
-      title: 'Are you sure?',
-      text: 'You may lose the added data after going back!',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, Go Back',
-      cancelButtonText: 'No, Stay Here',
-      customClass: {
-        popup: 'bg-tileLight dark:bg-tileBlack text-black dark:text-white',
-        title: 'font-semibold',
-      },
-      buttonsStyling: true,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        window.history.back();
-      }
-    });
+    if (!this.isView) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Are you sure?',
+        text: 'You may lose the added data after going back!',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, Go Back',
+        cancelButtonText: 'No, Stay Here',
+        customClass: {
+          popup: 'bg-tileLight dark:bg-tileBlack text-black dark:text-white',
+          title: 'font-semibold',
+        },
+        buttonsStyling: true,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.history.back();
+        }
+      });
+    } else {
+      window.history.back();
+    }
   }
 
   // Method for Sinhala name validation (block numbers and special characters)

1858802ec45d3011063354a77189370cb4b2d690
bimangunarathne <bimangunarathne@outlook.com>
2025-10-07 10:47:34 +0530

    update the code

---
+++ src/app/application/steckholders-section/edit-fieald-officer/edit-fieald-officer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-fieald-officer',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-fieald-officer.component.html',
+  styleUrl: './edit-fieald-officer.component.css'
+})
+export class EditFiealdOfficerComponent {
+
+}

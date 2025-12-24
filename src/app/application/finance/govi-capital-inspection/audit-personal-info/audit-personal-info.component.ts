143bb65edbd76a66865e3638eaaa559ee512a8ce
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-12-24 10:59:36 +0530

    update the code

--- src/app/application/finance/govi-capital-inspection/audit-personal-info/audit-personal-info.component.ts
+++ src/app/application/finance/govi-capital-inspection/audit-personal-info/audit-personal-info.component.ts
@@ -38,7 +38,7 @@ import { HarvestStorageTabComponent } from '../harvest-storage-tab/harvest-stora
 })
 export class AuditPersonalInfoComponent implements OnInit {
   isLoading: boolean = false;
-  activeTab: string = 'Personal';
+  activeTab: string = 'Investment';
   inspectionArray!: Inspection;
   reqId: number = 2;
 
--- src/app/application/finance/govi-capital-inspection/investment-info-tab/investment-info-tab.component.ts
+++ src/app/application/finance/govi-capital-inspection/investment-info-tab/investment-info-tab.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-investment-info-tab',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './investment-info-tab.component.html',
   styleUrl: './investment-info-tab.component.css'
 })
 export class InvestmentInfoTabComponent {
-
+  // Dummy data based on the image
+  investmentData = [
+    {
+      id: '01',
+      label: 'Expected investment by the farmer',
+      value: 'Rs. 1,8000,000,00',
+      type: 'text'
+    },
+    {
+      id: '02',
+      label: 'Purpose for investment required as per the farmer',
+      value: 'Buy agro input supplies',
+      type: 'text'
+    },
+    {
+      id: '03',
+      label: 'Expected repayment period as per the farmer in months',
+      value: '12',
+      type: 'text'
+    }
+  ];
 }

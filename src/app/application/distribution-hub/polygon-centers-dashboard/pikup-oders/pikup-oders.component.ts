85e54e930732cca3a20936be4e20e3ea6d35d5d2
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2026-01-07 14:16:12 +0530

    update the code

--- src/app/application/distribution-hub/polygon-centers-dashboard/pikup-oders/pikup-oders.component.ts
+++ src/app/application/distribution-hub/polygon-centers-dashboard/pikup-oders/pikup-oders.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { LoadingSpinnerComponent } from '../../../../components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-pikup-oders',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    DropdownModule,
+    CalendarModule,
+    LoadingSpinnerComponent,
+  ],
   templateUrl: './pikup-oders.component.html',
-  styleUrl: './pikup-oders.component.css'
+  styleUrl: './pikup-oders.component.css',
 })
 export class PikupOdersComponent {
-
+  isLoading = false;
 }

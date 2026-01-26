90151417245101e82070d25704576ee2a982ba83
bimangunarathne <bimangunarathne@outlook.com>
2026-01-26 17:29:06 +0530

    update the code

---
+++ src/app/application/finance/cultivation-history/cultivation-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cultivation-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './cultivation-history.component.html',
+  styleUrl: './cultivation-history.component.css'
+})
+export class CultivationHistoryComponent {
+
+}

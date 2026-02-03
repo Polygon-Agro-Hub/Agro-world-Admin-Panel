f8bfef9af7961993015ac777b21420960bd64cf9
Biman Gunarathne <bimangunarathne@outlook.com>
2026-02-03 16:25:48 +0530

    update the code

--- src/app/application/steckholders-section/view-collective-officer/view-collective-officer.component.ts
+++ src/app/application/steckholders-section/view-collective-officer/view-collective-officer.component.ts
@@ -238,6 +238,10 @@ export class ViewCollectiveOfficerComponent {
   }
 
   ngOnInit() {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Optional: adds smooth scrolling animation
+  });
     this.isSteckholdersRoute = this.router.url.includes('/steckholders/action/collective-officer');
     this.fetchAllCollectionOfficer(this.page, this.itemsPerPage);
     this.getAllcompany();

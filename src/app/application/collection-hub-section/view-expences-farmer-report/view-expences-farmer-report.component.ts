c52da44d1b23cffbd0596b61a27b5e2a1bc181de
Biman Gunarathne <bimangunarathne@outlook.com>
2026-03-17 12:58:56 +0530

    update the code

--- src/app/application/collection-hub-section/view-expences-farmer-report/view-expences-farmer-report.component.ts
+++ src/app/application/collection-hub-section/view-expences-farmer-report/view-expences-farmer-report.component.ts
@@ -427,7 +427,7 @@ export class ViewExpencesFarmerReportComponent implements OnInit {
     doc.text('Officer Qr Code', officerQrX + 6, labelY);
 
     // Save the PDF
-    doc.save(`invoice_${this.userObj.invNo}.pdf`);
+    doc.save(`Invoice_${this.userObj.invNo}.pdf`);
     this.isLoading = false;
   }
 

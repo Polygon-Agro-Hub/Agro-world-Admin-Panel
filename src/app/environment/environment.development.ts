dc97a5ae9db5723bc52def14731b958457031424
Polygon-Code-Hub <polygonholdingsdev@gmail.com>
2026-06-05 09:35:25 +0530

    Update API_URL for development environment

--- src/app/environment/environment.development.ts
+++ src/app/environment/environment.development.ts
@@ -5,11 +5,11 @@ export const environment = {
   // API_URL: 'https://polygonagro.com/admin-api/agro-api/admin-api/api/'
 
   //dev
-  // API_URL: 'https://dev.polygonagro.com/admin-api/agro-api/admin-api/api/',
+  API_URL: 'https://dev.polygonagro.com/admin-api/agro-api/admin-api/api/',
 
   //local
   // API_URL: 'http://localhost:3000/agro-api/admin-api/api/',
   SHOUTOUT_API_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwYjUxMWUzMC1mMjZjLTExZWYtOGQyZi1jNzhmOWJkMzU4NzciLCJzdWIiOiJTSE9VVE9VVF9BUElfVVNFUiIsImlhdCI6MTc0MDM3MzEzMywiZXhwIjoyMDU1OTA1OTMzLCJzY29wZXMiOnsiYWN0aXZpdGllcyI6WyJyZWFkIiwid3JpdGUiXSwibWVzc2FnZXMiOlsicmVhZCIsIndyaXRlIl0sImNvbnRhY3RzIjpbInJlYWQiLCJ3cml0ZSJdfSwic29fdXNlcl9pZCI6IjgzOTkzIiwic29fdXNlcl9yb2xlIjoidXNlciIsInNvX3Byb2ZpbGUiOiJhbGwiLCJzb191c2VyX25hbWUiOiIiLCJzb19hcGlrZXkiOiJub25lIn0.jqSNF1mJTmWHem1ZfjRQ1WKgsmB5N4tN8OOw7J9h6qI',
 
-  API_URL: 'https://admin-panel-api-uat.vercel.app/agro-api/admin-api/api/',
+  //API_URL: 'https://admin-panel-api-uat.vercel.app/agro-api/admin-api/api/',
 };

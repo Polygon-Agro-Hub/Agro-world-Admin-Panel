856d7b86068acbfc01076498db1e7b4b2575a57f
DumithLusena <dumithlusena123@gmail.com>
2026-07-28 09:27:08 +0530

    Add ShortageFinalizationToday component with HTML, CSS, and unit tests

---
+++ src/app/application/procurement-section/shortage/shortage-finalization-today/shortage-finalization-today.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shortage-finalization-today',
+  standalone: true,
+  imports: [],
+  templateUrl: './shortage-finalization-today.component.html',
+  styleUrl: './shortage-finalization-today.component.css'
+})
+export class ShortageFinalizationTodayComponent {
+
+}

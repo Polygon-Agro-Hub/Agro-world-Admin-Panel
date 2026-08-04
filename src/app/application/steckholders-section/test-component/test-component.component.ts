9b689b622c6e8a92dd63c164523d4ff1b3d1a6b5
Pasan-FDo <pasanfernando.work@gmail.com>
2026-08-04 16:41:37 +0530

    add socket.io-client integration and create TestComponent for connection status

---
+++ src/app/application/steckholders-section/test-component/test-component.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SocketService } from '../../../services/Socket/socket.service';
+
+@Component({
+  selector: 'app-test-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-component.component.html',
+  styleUrl: './test-component.component.css'
+})
+export class TestComponentComponent implements OnInit, OnDestroy {
+  isConnected = false;
+  socketId: string | undefined;
+  statusText = 'Disconnected';
+  private subscription: Subscription = new Subscription();
+
+  constructor(private socketService: SocketService) {}
+
+  ngOnInit() {
+    // Check connection status every 2 seconds
+    this.subscription.add(
+      this.socketService.onEvent('connect').subscribe(() => {
+        this.isConnected = true;
+        this.socketId = this.socketService.getSocketId();
+        this.statusText = 'Connected';
+      })
+    );
+
+    this.subscription.add(
+      this.socketService.onEvent('disconnect').subscribe(() => {
+        this.isConnected = false;
+        this.socketId = undefined;
+        this.statusText = 'Disconnected';
+      })
+    );
+
+    // Initial status check
+    setTimeout(() => {
+      this.isConnected = this.socketService.isConnected();
+      this.socketId = this.socketService.getSocketId();
+      this.statusText = this.isConnected ? 'Connected' : 'Disconnected';
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}

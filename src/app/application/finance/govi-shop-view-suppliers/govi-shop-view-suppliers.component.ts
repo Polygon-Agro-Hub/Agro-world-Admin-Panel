bdd13c456888fcf4934d0b7163ef6e758a60a333
DumithLusena <dumithlusena123@gmail.com>
2026-08-21 15:56:57 +0530

    Remove debug console logs and improve button styling for calendar tasks

--- src/app/application/finance/govi-shop-view-suppliers/govi-shop-view-suppliers.component.ts
+++ src/app/application/finance/govi-shop-view-suppliers/govi-shop-view-suppliers.component.ts
@@ -140,7 +140,6 @@ export class GoviShopViewSuppliersComponent implements OnInit {
 
   onPageChange(event: number) {
     this.page = event;
-    console.log('page', this.page)
     this.fetchAllOrders();
   }
 

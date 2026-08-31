22e9fdea29c35f544e556881cc7a217e113341f6
BimanGunarathne <bimangunarathne@outlook.com>
2026-08-31 11:48:27 +0530

    feat: add CommonModule import to CompletedViewAllOdersComponent

--- src/app/application/finance/sales/completed-view-all-oders/completed-view-all-oders.component.ts
+++ src/app/application/finance/sales/completed-view-all-oders/completed-view-all-oders.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-completed-view-all-oders',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './completed-view-all-oders.component.html',
   styleUrl: './completed-view-all-oders.component.css'
 })

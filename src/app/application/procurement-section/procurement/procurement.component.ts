d6647a20948902433696ef3ca080d182239b702f
Tharaka Nuwan <99677288+ThareDev@users.noreply.github.com>
2026-09-15 16:23:40 +0530

    Load Mismatching Reports Today frontend

--- src/app/application/procurement-section/procurement/procurement.component.ts
+++ src/app/application/procurement-section/procurement/procurement.component.ts
@@ -21,6 +21,13 @@ export class ProcurementComponent {
     public permissionService: PermissionService
   ) { }
 
+  istogglePopupLoadMismatchView = false;
+  pendingLoadAlertsCount = 3; // replace with real value from your service later
+
+  togglePopupLoadMismatch() {
+    this.istogglePopupLoadMismatchView = !this.istogglePopupLoadMismatchView;
+  }
+
   togglePopupProductStorage(): void {
     this.istogglePopupProductStorageView = !this.istogglePopupProductStorageView;
   }
@@ -38,6 +45,10 @@ export class ProcurementComponent {
   }
   dailypackingtarget(): void {
     this.router.navigate(['/procurement/daily-packing-target']);
+  }
+
+    loadmismatch(): void {
+    this.router.navigate(['/procurement/pending-procurement-product-mismatch-today']);
   }
 
   navigatePath(path: string) {

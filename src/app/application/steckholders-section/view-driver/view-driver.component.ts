8599ff4087805b7f24a60605b2dd14ad7d6bce2a
DumithLusena <dumithlusena123@gmail.com>
2026-09-15 11:11:34 +0530

    feat: add driver role filter and update service to handle driver role in collection requests

--- src/app/application/steckholders-section/view-driver/view-driver.component.ts
+++ src/app/application/steckholders-section/view-driver/view-driver.component.ts
@@ -84,6 +84,7 @@ export class ViewDriverComponent implements OnInit {
   selectCenterStatus: string = '';
   selectStatus: string = '';
   selectCategory: string = '';
+  selectDriverRole: string = '';
   isSteckholdersRoute: boolean = false;
 
   hasData: boolean = false;
@@ -98,6 +99,11 @@ export class ViewDriverComponent implements OnInit {
     { label: 'Approved', value: 'Approved' },
     { label: 'Not Approved', value: 'Not Approved' },
     { label: 'Rejected', value: 'Rejected' },
+  ];
+
+  driverRoleOptions = [
+    { label: 'Light Weight Driver', value: 'Light Weight Driver' },
+    { label: 'Heavy Weight Driver', value: 'Heavy Weight Driver' },
   ];
 
   centerOptions: CenterOptions[] = [];
@@ -134,7 +140,8 @@ export class ViewDriverComponent implements OnInit {
     status: string = this.selectStatus,
     searchNIC: string = this.searchNIC,
     centerId: number | null = this.cId,
-    driverCatId: string | null = this.selectCategory || null
+    driverCatId: string | null = this.selectCategory || null,
+    driverRole: string = this.selectDriverRole
   ) {
     this.isLoading = true;
     this.collectionService
@@ -145,7 +152,8 @@ export class ViewDriverComponent implements OnInit {
         status,
         searchNIC,
         centerId,
-        driverCatId ? Number(driverCatId) : null
+        driverCatId ? Number(driverCatId) : null,
+        driverRole
       )
       .subscribe(
         (response) => {

093e7390b6b1d30e80c5a04faa3a55b41b9774a5
DumithLusena <dumithlusena123@gmail.com>
2026-09-16 12:07:10 +0530

    fix: Update transaction confirmation message and improve date handling in transactions

--- src/app/application/finance/transaction-amount/transaction-amount.component.ts
+++ src/app/application/finance/transaction-amount/transaction-amount.component.ts
@@ -48,7 +48,7 @@ export class TransactionAmountComponent implements OnInit {
       .subscribe(
         (response) => {
           this.isLoading = false;
-          this.total = response.total
+          this.total = response.total;
           this.orders = response.orders;
           this.hasData = this.orders.length > 0;
           this.totalItems = this.orders.length;

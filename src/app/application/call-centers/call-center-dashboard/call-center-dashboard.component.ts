e71b26f991b8ffa659ed74960440f1e40bbb4de1
BimanGunarathne <bimangunarathne@outlook.com>
2026-09-24 13:39:06 +0530

    Add ColumRightComponent with template, styles, and tests to call center dashboard

--- src/app/application/call-centers/call-center-dashboard/call-center-dashboard.component.ts
+++ src/app/application/call-centers/call-center-dashboard/call-center-dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RowOneComponent } from './row-one/row-one.component';
+import { ColumRightComponent } from './colum-right/colum-right.component';
 
 @Component({
   selector: 'app-call-center-dashboard',
   standalone: true,
-  imports: [RowOneComponent],
+  imports: [RowOneComponent, ColumRightComponent],
   templateUrl: './call-center-dashboard.component.html',
   styleUrl: './call-center-dashboard.component.css',
 })
---
+++ src/app/application/call-centers/call-center-dashboard/colum-right/colum-right.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colum-right',
+  standalone: true,
+  imports: [],
+  templateUrl: './colum-right.component.html',
+  styleUrl: './colum-right.component.css'
+})
+export class ColumRightComponent {
+
+}

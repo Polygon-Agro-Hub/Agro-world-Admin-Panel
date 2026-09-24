3011a05de141139f0776da5e4b812bb2967f5ca4
BimanGunarathne <bimangunarathne@outlook.com>
2026-09-24 10:50:51 +0530

    Add RowOneComponent with template, styles, and tests

---
+++ src/app/application/call-centers/call-center-dashboard/row-one/row-one.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-row-one',
+  standalone: true,
+  imports: [],
+  templateUrl: './row-one.component.html',
+  styleUrl: './row-one.component.css'
+})
+export class RowOneComponent {
+
+}
